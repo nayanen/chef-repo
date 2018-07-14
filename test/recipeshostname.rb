3b745c7c58710cea3221ab7cf607dd5496a2c999
Nayanendu Mishra <36026937+nayanen@users.noreply.github.com>
2018-07-14 13:56:04 +0530

    Create recipeshostname.rb

---
+++ test/recipeshostname.rb
@@ -0,0 +1,13 @@
+{
+    "opsworks": {
+        "layers": {
+            "test": {
+                "instances": {
+                    "xyxx1": {
+                        "private-ip": "172.31.25.249"
+                    }
+                }
+            }
+        }
+    }
+}
